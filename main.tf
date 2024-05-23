12e77263bcd029d995a98720ae6ae1999bcb9a61
Mufaddalone <48063315+Mufaddalone@users.noreply.github.com>
2024-05-22 18:51:01 -0700

    added tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0cbe318e714fc9a82"  # Change to your preferred AMI ID
+  instance_type = "t2.micro"
+  key_name      = "website"  # Reference the existing key pair name
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "website"
+  }
+}
